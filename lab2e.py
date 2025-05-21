7c0d571048d9f865817631b7c88e2c48e027f0fb
Daniel Rhodes <drhodes2@myseneca.ca>
2025-05-20 21:29:28 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: Daniel Rhodes
+#Author ID: 130312234
+#Date Created: 2025/05/20
+
+timer = int(10)
+
+while timer != 0:
+    print(str(timer))
+    timer = timer - 1
+
+print('blast off!')
