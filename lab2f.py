d7a5e053c919e41df79983ae407cfa5e5cfe4797
Daniel Rhodes <drhodes2@myseneca.ca>
2025-05-20 21:38:27 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Author: Daniel Rhodes
+#Author ID: 130312234
+#Date Created: 2025/05/20
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
